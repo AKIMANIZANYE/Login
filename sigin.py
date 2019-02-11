600a33a3ae662705e9cd3523c90a5b8ffc64b204
Claudine Akimanizanye <akimanizanye.claudine@gmail.com>
2019-02-11 14:04:04 +0200

    first commit

---
+++ sigin.py
@@ -0,0 +1,13 @@
+
+#This is for creating username  and password
+a=input()# for username
+b=input()#for passwword
+e= int(input())# for phone number
+#this  will check the created account
+c=input() #for username
+d=input()#for password
+if(a==c and b==d):
+  print("login into your account")
+else:
+    print("incorrect username or password")
+
